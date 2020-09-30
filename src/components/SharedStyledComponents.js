a19e8b4fb12e340efeb8e8af94c3d6131b5a27ef
Sam Richards <sbrichards@gmail.com>
2020-09-30 10:10:28 -0700

    Add state to conditionally display address

--- src/components/SharedStyledComponents.js
+++ src/components/SharedStyledComponents.js
@@ -163,7 +163,7 @@ export const StyledCardMaxWidth = styled(StyledCard)`
 `
 
 // Fake buttons
-
+// TODO rename to `Button` & `ButtonLink`?
 export const FakeButton = styled.div`
   text-decoration: none;
   display: inline-block;
